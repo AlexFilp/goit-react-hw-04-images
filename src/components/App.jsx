2be57dc5f9888fb508534a199c31288181d313a0
Oleksandr FIlippov <filya170895@gmail.com>
2023-03-05 14:42:11 +0100

    123

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,40 +1,65 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 
-export class App extends Component {
-  state = {
-    query: '',
-    page: 1,
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+
+  const handleSearchSubmit = query => {
+    console.log(query);
+    setQuery(query);
+    setPage(1);
   };
 
-  handleSearchSubmit = query => {
-    this.setState({ query, page: 1 });
+  const loadMore = () => {
+    setPage(prevState => prevState + 1);
   };
 
-  loadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
-  };
+  return (
+    <div className="App">
+      <Searchbar onSubmit={handleSearchSubmit} />
+      <ImageGallery query={query} page={page} onLoad={loadMore} />
 
-  render() {
-    return (
-      <div className="App">
-        <Searchbar onSubmit={this.handleSearchSubmit} />
-        <ImageGallery
-          query={this.state.query}
-          page={this.state.page}
-          onLoad={this.loadMore}
-        />
+      <ToastContainer position="top-right" autoClose={2000} limit={3} />
+    </div>
+  );
+};
 
-        <ToastContainer position="top-right" autoClose={2000} limit={3} />
-      </div>
-    );
-  }
-}
+// export class OldApp extends Component {
+//   state = {
+//     query: '',
+//     page: 1,
+//   };
+
+//   handleSearchSubmit = query => {
+//     console.log(query);
+//     this.setState({ query, page: 1 });
+//   };
+
+//   loadMore = () => {
+//     this.setState(prevState => ({
+//       page: prevState.page + 1,
+//     }));
+//   };
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <Searchbar onSubmit={this.handleSearchSubmit} />
+//         <ImageGallery
+//           query={this.state.query}
+//           page={this.state.page}
+//           onLoad={this.loadMore}
+//         />
+
+//         <ToastContainer position="top-right" autoClose={2000} limit={3} />
+//       </div>
+//     );
+//   }
+// }
 
 //  async componentDidMount() {
 //     const url = `${this.#BASE_URL}?q=cat&page=1&key=${
--- src/components/Searchbar/SearchForm.jsx
+++ src/components/Searchbar/SearchForm.jsx
@@ -1,44 +1,81 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import css from './Searchbar.module.css';
 import { toast } from 'react-toastify';
 import { FcSearch } from 'react-icons/fc';
 
-export class SearchForm extends Component {
-  state = {
-    searchQuery: '',
+export const SearchForm = ({ onSubmit }) => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleQueryChange = evt => {
+    setSearchQuery(evt.target.value);
   };
 
-  handleQueryChange = evt => {
-    this.setState({ searchQuery: evt.currentTarget.value.toLowerCase() });
-  };
-
-  handleSubmit = evt => {
+  const handleSubmit = evt => {
     evt.preventDefault();
-    if (this.state.searchQuery.trim() === '') {
+
+    if (searchQuery.trim() === '') {
       return toast.error('Please write something!');
     }
-    this.props.onSubmit(this.state.searchQuery);
-    this.setState({ searchQuery: '' });
+    onSubmit(searchQuery);
+    setSearchQuery('');
   };
 
-  render() {
-    return (
-      <form className={css.SearchForm} onSubmit={this.handleSubmit}>
-        <button type="submit" className={css.SearchFormButton}>
-          <FcSearch />
-        </button>
+  return (
+    <form className={css.SearchForm} onSubmit={handleSubmit}>
+      <button type="submit" className={css.SearchFormButton}>
+        <FcSearch />
+      </button>
 
-        <input
-          onChange={this.handleQueryChange}
-          name="searchQuery"
-          value={this.state.searchQuery}
-          className={css.SearchFormInput}
-          type="text"
-          autoComplete="off"
-          autoFocus
-          placeholder="Search images and photos"
-        />
-      </form>
-    );
-  }
-}
+      <input
+        onChange={handleQueryChange}
+        name="searchQuery"
+        value={searchQuery}
+        className={css.SearchFormInput}
+        type="text"
+        autoComplete="off"
+        autoFocus
+        placeholder="Search images and photos"
+      />
+    </form>
+  );
+};
+
+// export class OldSearchForm extends Component {
+//   state = {
+//     searchQuery: '',
+//   };
+
+//   handleQueryChange = evt => {
+//     this.setState({ searchQuery: evt.currentTarget.value.toLowerCase() });
+//   };
+
+//   handleSubmit = evt => {
+//     evt.preventDefault();
+//     if (this.state.searchQuery.trim() === '') {
+//       return toast.error('Please write something!');
+//     }
+//     this.props.onSubmit(this.state.searchQuery);
+//     this.setState({ searchQuery: '' });
+//   };
+
+//   render() {
+//     return (
+//       <form className={css.SearchForm} onSubmit={this.handleSubmit}>
+//         <button type="submit" className={css.SearchFormButton}>
+//           <FcSearch />
+//         </button>
+
+//         <input
+//           onChange={this.handleQueryChange}
+//           name="searchQuery"
+//           value={this.state.searchQuery}
+//           className={css.SearchFormInput}
+//           type="text"
+//           autoComplete="off"
+//           autoFocus
+//           placeholder="Search images and photos"
+//         />
+//       </form>
+//     );
+//   }
+// }
